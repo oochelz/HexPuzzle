56831cd05d12ba89dffa6cc066f240fa872c7b4f
Chelsea Youmans <chelsea@oochelz.com>
2023-07-09 18:49:25 -0400

    Initial Commit

---
+++ HexPuzzle/HexPuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexPuzzleApp.swift
+//  HexPuzzle
+//
+//  Created by Chelsea Youmans on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct HexPuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
